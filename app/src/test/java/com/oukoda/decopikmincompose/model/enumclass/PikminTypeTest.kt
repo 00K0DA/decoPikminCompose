f431315399dbd16166cef158493a05586969dc0d
00K0DA <yushi.ookoda.tuba@gmail.com>
2023-01-17 23:14:55 +0900

    Add PikminTypeTest

---
+++ app/src/test/java/com/oukoda/decopikmincompose/model/enumclass/PikminTypeTest.kt
@@ -0,0 +1,31 @@
+package com.oukoda.decopikmincompose.model.enumclass
+
+import androidx.compose.ui.graphics.Color
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PikminTypeTest {
+    @Test
+    fun isColorUnique() {
+        val colors: List<Color> = PikminType.values().map {
+            it.color()
+        }
+
+        colors.forEach { color ->
+            val stringIdCount = colors.count { it == color }
+            assertEquals(1, stringIdCount)
+        }
+    }
+
+    @Test
+    fun isStringIdUnique() {
+        val stringIds: List<Int> = PikminType.values().map {
+            it.stringId()
+        }
+
+        stringIds.forEach { stringId ->
+            val stringIdCount = stringIds.count { it == stringId }
+            assertEquals(1, stringIdCount)
+        }
+    }
+}
