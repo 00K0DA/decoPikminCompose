71e15d56ef7bce126a426762bf473afd8b93a749
00K0DA <yushi.ookoda.tuba@gmail.com>
2023-01-22 21:45:02 +0900

    ローディング画面の追加

--- app/src/main/java/com/oukoda/decopikmincompose/MainActivity.kt
+++ app/src/main/java/com/oukoda/decopikmincompose/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.VisibleForTesting
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +18,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,18 +60,35 @@ class MainActivity : ComponentActivity() {
     fun MainScreen(mainViewModel: MainViewModel) {
         val decors by mainViewModel.decorGroups.collectAsState()
         mainViewModel.createDecors()
+        val isLoading = mainViewModel.isLoading.observeAsState().value!!
         DecoPikminComposeTheme {
             // A surface container using the 'background' color from the theme
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background,
             ) {
-                Column {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    CreatePikminDecorView(decors) { pikminRecord ->
-                        mainViewModel.updatePikminRecord(pikminRecord)
+                Box() {
+                    Column {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CreatePikminDecorView(decors) { pikminRecord ->
+                            mainViewModel.updatePikminRecord(pikminRecord)
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
                     }
-                    Spacer(modifier = Modifier.height(16.dp))
+
+                    AnimatedVisibility(
+                        visible = isLoading,
+                        exit = fadeOut(),
+                    ) {
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .background(Color.DarkGray.copy(alpha = 0.9F)),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Text(text = "Loading", color = Color.White)
+                        }
+                    }
                 }
             }
         }
